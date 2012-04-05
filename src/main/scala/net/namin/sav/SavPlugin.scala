6b00a569423f32902edcfae62f64ad5ebcc99dfa
Nada Amin <namin@alum.mit.edu>
2012-04-06 01:08:31 +0200

    added the lock/unlock case from lecture

--- src/main/scala/net/namin/sav/SavPlugin.scala
+++ src/main/scala/net/namin/sav/SavPlugin.scala
@@ -84,8 +84,8 @@ trait Sav extends PluginComponent {
     private var ok = true
     private var labels = Map[Name,(Vertex,Vertex)]()
     private var next = cfg.newVertex
-    private var nextLabel = cfg.error
-    private var afterNextLabel = cfg.error
+    private var lastAssertFrom = cfg.error
+    private var lastAssertTo = cfg.error
     cfg.start = next
     
     private def newNext = {
@@ -104,19 +104,23 @@ trait Sav extends PluginComponent {
       cfg.asserts += (from -> e)
       cfg += (from, Assume(e), to)
       cfg += (from, Assume(simplify(Not(e))), cfg.error)
-      nextLabel = from
-      afterNextLabel = to
+      lastAssertFrom = from
+      lastAssertTo = to
     }
 
     private def addAssign(v: String, rhs: Expression) = {
       addEdge(lazabs.cfg.Assign(Variable(v), rhs))
     }
 
-    private def addLabel(n: Name) = {
-      assert(nextLabel != cfg.error && afterNextLabel == next, "while loop must be preceded by assert!")
-      labels += (n -> (nextLabel, afterNextLabel))
-      nextLabel = cfg.error
-      afterNextLabel = cfg.error
+    private def addWhileLabel(n: Name) = {
+      assert(lastAssertFrom != cfg.error && lastAssertTo == next, "while loop must be preceded by assert!")
+      labels += (n -> (lastAssertFrom, lastAssertTo))
+      lastAssertFrom = cfg.error
+      lastAssertTo = cfg.error
+    }
+    
+    private def addDoWhileLabel(n: Name) = {
+      labels += (n -> (next, next))
     }
 
     private def jumpTo(v: Vertex) = {
@@ -141,26 +145,36 @@ trait Sav extends PluginComponent {
           cfg.variables += v
           addAssign(v, expr(rhs))
         case LabelDef(name, List(), rhs @ If(cond, thenp, Literal(Constant(())))) =>
-          addLabel(name)
+          addWhileLabel(name)
           val conde = expr(cond)
           addEdge(Assume(conde))
           traverse(thenp)
           assert(labels(name)._2 == next)
           addEdge(Assume(simplify(Not(conde))))
-        case If(cond, thenp, elsep) =>
+        case LabelDef(name, List(), rhs @ Block(stats, If(cond, thenp, Literal(Constant(()))))) =>
+          addDoWhileLabel(name)
+          traverseTrees(stats)
           val from = next
           val conde = expr(cond)
           addEdge(Assume(conde))
           traverse(thenp)
-          val end = next
+          assert(labels(name)._2 == next)
           next = from
           addEdge(Assume(simplify(Not(conde))))
+        case If(cond, thenp, elsep) =>
+          val from = next
+          val conde = exprIfOk(cond)
+          conde.foreach(e => addEdge(Assume(e)))
+          traverse(thenp)
+          val end = next
+          next = from
+          conde.foreach(e => addEdge(Assume(simplify(Not(e)))))
           traverse(elsep)
           jumpTo(end)
-        case Assign(Ident(name), Apply(Select(_, fun), List())) if fun.decode == "havoc" =>
-          addEdge(Havoc(Variable(name.decode)))
-        case Assign(Ident(name), rhs) =>
-          addAssign(name.decode, expr(rhs))
+        case Assign(Ident(name), rhs) if cfg.variables.contains(name.decode) => exprIfOk(rhs) match {
+          case None => addEdge(Havoc(Variable(name.decode)))
+          case Some(e) => addAssign(name.decode, e)
+        }
         case Apply(Select(_, fun), List(arg)) if fun.decode == "assume" =>
           addEdge(Assume(expr(arg)))
         case Apply(Select(_, fun), List(arg)) if fun.decode == "assert" =>
@@ -169,29 +183,47 @@ trait Sav extends PluginComponent {
           jumpTo(labels(name)._1)
           next = labels(name)._2
         case Literal(Constant(())) => ()
+        case Apply(Select(_, fun), List(arg)) if fun.decode.endsWith("_=")=>
+          println("ignoring complex assignment " + t)
         case _ =>
-          println("missing case: " + t.getClass + ":" + t)
+          println("missing case: " + t.getClass + " " + t)
           ok = false
       }
     }
-        
-    def expr(t: Tree) = simplify((new ExpressionBuilder).build(t))
-    class ExpressionBuilder {
+    
+    def exprIfOk(t: Tree) = {
+      val exprBuilder = new ExpressionBuilder(false)
+      val expr = exprBuilder.build(t)
+      if (exprBuilder.ok) Some(simplify(expr)) else None 
+    }
+    def expr(t: Tree) = {
+      val exprBuilder = new ExpressionBuilder(true)
+      val expr = exprBuilder.build(t)
+      if (exprBuilder.ok) simplify(expr)
+      else { ok = false; expr }
+    }
+    class ExpressionBuilder(signalError: Boolean) {
+      var ok = true
       def build(t: Tree): Expression = t match {
         case Literal(Constant(value)) => value match {
           case x : Int => NumericalConst(x)
           case x : Boolean => BoolConst(x)
         }
-        case Ident(name) => Variable(name.decode)
-        case Apply(Select(lhs, op), List(rhs)) =>
+        case Ident(name) if cfg.variables.contains(name.decode) => Variable(name.decode)
+        case Apply(Select(e, op), List()) if unaryOps.contains(op.decode) =>
+          UnaryExpression(unaryOps(op.decode), build(e))
+        case Apply(Select(lhs, op), List(rhs)) if binaryOps.contains(op.decode) =>
           BinaryExpression(build(lhs), binaryOps(op.decode), build(rhs))
         case _ =>
-          println("missing case in expression builder: " + t.getClass)
+          if (signalError) println("missing case in expression builder: " + t.getClass + " " + t)
           ok = false
           null
       }
     }
   }
+
+  val unaryOps = Map(
+    "unary_!" -> NotOp())
 
   val binaryOps = Map(
     "||" -> DisjunctionOp(),
---
+++ test/ex4.scala
@@ -0,0 +1,39 @@
+object ex4 {
+  class Queue(var data: Int, var next: Option[Queue])
+
+  def lock() = 1
+  def unlock() = 0
+
+  def test(queue: Queue) {
+    var locked = 0
+
+    var oldi = 0
+    var newi = 0
+    var q = queue
+
+    do {
+      assert(locked == 0)
+      locked = lock()
+      assume(locked == 1)
+
+      oldi = newi
+
+      if (q.next != None) {
+        q = q.next.get
+        q.data = newi
+
+        assert(locked == 1 && newi == oldi)
+        locked = unlock()
+        assume(locked == 0)
+
+        newi += 1
+      }
+
+      assert((locked == 0 && newi != oldi) || (locked == 1 && newi == oldi))
+    } while (newi != oldi)
+
+    assert(locked == 1)
+    locked = unlock()
+    assume(locked == 0)
+  }
+}
