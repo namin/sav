846a43d339f23bd919a510d963cb6532842ee9a7
Nada Amin <namin@alum.mit.edu>
2012-04-10 22:54:40 +0200

    another invalid example: currently, this one generates warnings about aliases without failing

---
+++ test/inv3.scala
@@ -0,0 +1,23 @@
+import net.namin.sav.annotation.verify
+import net.namin.sav.lib._
+
+object inv2 {
+  @verify
+  class Foo {
+    var x = 0
+  }
+
+  @verify
+  class Bar {
+    val foo = new Foo
+
+    @verify
+    def baz() {
+      val foo_alias = foo
+      foo.x = 1
+      foo_alias.x = 2
+      // postcondition is always false
+      postcondition(foo.x == 1)
+    }
+  }
+}
